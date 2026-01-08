f0559fd9670cf1ca681c19ff068821774c0f5c27
Olha <189846437+sibaraku@users.noreply.github.com>
2026-01-08 12:16:26 +0200

    Display all articles

--- index.js
+++ index.js
@@ -1,39 +1,45 @@
-const Express = require('express')
-const mongodb = require('mongodb').MongoClient
-const dotenv = require('dotenv')
-dotenv.config()
-const hbs = require("express-handlebars")
-const path = require('path')
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const dotenv = require('dotenv');
+const hbs = require('express-handlebars');
+const path = require('path');
 
-const app = Express()
+dotenv.config();
 
-app.set("views", path.join(__dirname, "views"))
-app.set("view engine", "hbs")
+const app = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
 app.engine("hbs", hbs.engine({
-    extname: "hbs",
-    defaultLayout: "main",
-    layoutsDir: __dirname + "/views/layouts/"
-}))
+  extname: "hbs",
+  defaultLayout: "main",
+  layoutsDir: path.join(__dirname, "views/layouts"),
+}));
 
-connectionToDB = async (connectionString) => {
-  try {
-    const client = await mongodb.connect(connectionString);
-    db = client.db();
-    console.log('Connected to MongoDb');
-  } catch (err) {
-    console.error('Failed to connect to MongoDb', err);
-  }
-  return db;
-}
+app.use(express.static("public"));
 
-const user = process.env.MONGO_USER
-const password = process.env.MONGO_PASSWORD
-const host = process.env.MONGO_HOST
-const port = process.env.MONGO_PORT
-const dbname = process.env.MONGO_DATABASE
+const connectToDB = async (connectionString) => {
+  const client = await MongoClient.connect(connectionString);
+  console.log("Connected to MongoDB");
+  return client.db();
+};
 
-let db, connectionString = `mongodb://${host}:${port}/${dbname}`
-connectionToDB(connectionString)
+const host = process.env.MONGO_HOST;
+const mongoPort = process.env.MONGO_PORT;
+const dbname = process.env.MONGO_DATABASE;
+
+let db;
+const connectionString = `mongodb://${host}:${mongoPort}/${dbname}`;
+
+;(async () => {
+  db = await connectToDB(connectionString);
+})();
+
+app.get("/", async (req, res) => {
+  const articles = await db.collection('articles').find().toArray();
+  res.render("index", { articles });
+});
+
 app.listen(3012, () => {
   console.log('Server is running on port 3012');
-})
+});
