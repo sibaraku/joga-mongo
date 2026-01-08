e70b1058b0e79039a026003bfb9528dbbcd4aa65
Olha <189846437+sibaraku@users.noreply.github.com>
2026-01-08 12:22:13 +0200

    Display an article by slug

--- index.js
+++ index.js
@@ -40,6 +40,12 @@ app.get("/", async (req, res) => {
   res.render("index", { articles });
 });
 
+app.get("/article/:slug", async (req, res) => {
+    const article = await (await db).collection("articles").findOne({slug: req.params.slug})
+    console.log(article)
+    res.render("article", {article: article})
+})
+
 app.listen(3012, () => {
   console.log('Server is running on port 3012');
 });
